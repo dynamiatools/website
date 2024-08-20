d60dc07f90e850977a5a12fd49857c2a43b2c77b
Mario Serrano Leones <mario@dynamiasoluciones.com>
2024-08-20 12:28:37 -0500

    getting started guide

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,48 @@
 import {defineConfig} from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+export const dynamiaToolsVersion = '5.2.0-beta'
+
+
 // https://astro.build/config
 export default defineConfig({
+    vite: {
+        define: {
+            _TOOLS_VERSION_: JSON.stringify(dynamiaToolsVersion)
+        }
+    },
     integrations: [
         starlight({
             title: 'DynamiaTools',
+            editLink: {
+                baseUrl: 'https://github.com/dynamiatools/website',
+            },
             social: {
                 github: 'https://github.com/dynamiatools/framework',
             },
-            defaultLocale: 'en',
+
             locales: {
-                en: {
-                    label: 'English'
-                },
-                es: {
-                    label: 'Español'
-                }
+                root: {label: 'English', lang: 'en'},
+                es: {label: 'Español', lang: 'es'}
             },
             sidebar: [
-
+                {
+                    label: 'Start Here',
+                    translations: {
+                        'es': 'Comieza Aqui'
+                    },
+                    items: [
+                        'getting-started',
+                        'custom-setup'
+                    ]
+                },
+                {
+                    label: 'Guides',
+                    translations: {
+                        es: 'Guías',
+                    },
+                    autogenerate: {directory: 'guides'},
+                }
             ],
         }),
     ],
